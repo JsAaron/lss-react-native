3e468d19efd2d158ccb4dcac55edfcc4f4ec8891
jsAaron <401545553@qq.com>
2019-03-17 19:59:49 +0800

    123

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  */
 
 import {AppRegistry} from 'react-native';
-import App from './src/main';
+import App from './src/test';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
