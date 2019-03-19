90868fefd9e81fd83a44b30caf7181aedb4a5825
jsaaron <401545553@qq.com>
2019-03-19 09:34:16 +0800

    123

--- src/demo/view.js
+++ src/demo/view.js
@@ -1,19 +1,74 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
-export default class ViewColoredBoxesWithText extends Component {
+export default class TextInANest extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      titleText: "Bird's Nest",
+      bodyText: 'This is not really a bird nest.'
+    };
+  }
+
   render() {
     return (
-      <View
-        style={{
-          flexDirection: 'row',
-          height: 150,
-          padding: 10,
-        }}
-      >
-        <View style={{ backgroundColor: 'blue', flex: 1 }} />
-        <View style={{ backgroundColor: 'red', flex: 1 }} />
+      <View>
+        <Text style={styles.baseText}>
+          <Text style={styles.titleText} onPress={this.onPressTitle}>
+            {this.state.titleText}
+            {'\n'}
+            {'\n'}
+          </Text>
+          <Text numberOfLines={5}>{this.state.bodyText}</Text>
+          <Text
+            style={{
+              fontWeight: 'bold'
+            }}
+          >
+            I am bold
+            <Text
+              style={{
+                color: 'red'
+              }}
+            >
+              and red
+            </Text>
+          </Text>
+        </Text>
+        <Text style={styles.baseText}>
+          <Text style={styles.titleText} onPress={this.onPressTitle}>
+            {this.state.titleText}
+            {'\n'}
+            {'\n'}
+          </Text>
+          <Text numberOfLines={5}>{this.state.bodyText}</Text>
+          <Text
+            style={{
+              fontWeight: 'bold'
+            }}
+          >
+            I am bold
+            <Text
+              style={{
+                color: 'red'
+              }}
+            >
+              and red
+            </Text>
+          </Text>
+        </Text>
+        <Button title="提交" color="#841584" />
       </View>
-    )
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  baseText: {
+    fontFamily: 'Cochin'
+  },
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold'
+  }
+});
