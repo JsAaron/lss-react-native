35abb34eb7db9c54037c9ca18d89976d2151fb8c
jsaaron <401545553@qq.com>
2019-03-19 09:08:12 +0800

    123

--- src/demo/view.js
+++ src/demo/view.js
@@ -7,13 +7,12 @@ export default class ViewColoredBoxesWithText extends Component {
       <View
         style={{
           flexDirection: 'row',
-          height: 100,
-          padding: 20,
+          height: 150,
+          padding: 10,
         }}
       >
-        <View style={{ backgroundColor: 'blue', flex: 0.3 }} />
-        <View style={{ backgroundColor: 'red', flex: 0.5 }} />
-        <Text>Hello World!</Text>
+        <View style={{ backgroundColor: 'blue', flex: 1 }} />
+        <View style={{ backgroundColor: 'red', flex: 1 }} />
       </View>
     )
   }
